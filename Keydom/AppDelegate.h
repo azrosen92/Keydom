aa662a20ae584b26363a35e494538f7b41e995e6
jruberg <rubergly@gmail.com>
2012-05-03 18:57:55 -0400

    Initial Commit

---
+++ Keydom/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Keydom
+//
+//  Created by Jeffrey Ruberg on 5/3/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Keydom/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Keydom
+//
+//  Created by Jeffrey Ruberg on 5/3/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
